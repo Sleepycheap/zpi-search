bf453dc94d7c417c2378d3f9b1b703924a88d676
Sleepycheap <anthony.vaut@gmail.com>
2026-02-26 11:48:54 -0600

    remove env

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,8 +3,14 @@ import { search } from '../utils/egnyte.js';
 
 export async function searchResults(req, res) {
   // const r = req;
+  const params = {
+    type: req.body.type,
+    operator: req.body.operator,
+    folder: req.body.folder,
+  };
+  // const sParams = new URLSearchParams(params).toString();
   const { query } = req.body;
-  const results = await search(query);
+  const results = await search(query, params);
   console.log('Result', results);
   res.render('showResults', {
     results: results,
